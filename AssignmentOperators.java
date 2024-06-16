b82b0d52cf2bf2fd9a967aa27c5280c6b9b42c59
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-06-16 17:56:41 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,10 @@
+class AssignmentOperators
+{
+	public static void main(String args[])		
+	{
+		int a=10;
+		System.out.println("'=' is an Assignment Operator used to assign a value to a variable");
+		System.out.println("\n Below are other Assignment Operators which are also called ShortHand Operators\n");
+		System.out.println(" += \n -= \n *= \n /= \n %=");
+	}
+}
