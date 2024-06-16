33811c55a41e34a7b21c8f94a29ce91336cf966c
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-06-16 17:41:16 +0530

    Add files via upload

---
+++ DefaultValueOfDatatypes.java
@@ -0,0 +1,27 @@
+class DefaultValueOfDatatypes
+{
+	static byte a;
+	static char b;
+	static int c;
+	static float d;
+	static double e;
+	static long f;
+	static short g;
+	static boolean h;
+	public static void main(String args[])
+	{
+		System.out.println("Below are the default value of all datatypes present in java \n");
+		System.out.println("Byte = "+a);
+		System.out.println("Character = "+b);
+		System.out.println("Integer = "+c);
+		System.out.println("Float = "+d);
+		System.out.println("Double = "+e);
+		System.out.println("Long = "+f);
+		System.out.println("Short = "+g);
+		System.out.println("Boolean = "+h);
+	}
+}
+		
+
+	
+	
