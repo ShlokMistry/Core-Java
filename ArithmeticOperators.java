67a23f5ca5089c90ff29cc6c0a00c4df6f46b387
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-06-16 17:48:52 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,13 @@
+class ArithmeticOperators
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 20;
+		System.out.println("Addition of "+a+" & "+b+" is "+(a+b));
+		System.out.println("Subtraction of "+a+" & "+b+" is "+(a-b));
+		System.out.println("Multiplication of "+a+" & "+b+" is "+(a*b));
+		System.out.println("Division of "+a+" & "+b+" is "+(a/b));
+		System.out.println("Modulus(Remainder) of "+a+" & "+b+" is "+(a%b));
+	}
+}
