3e736fdd6a1e01cd522aaf92e7534e11e285a94f
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-06-17 12:08:45 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+class TernaryOperator
+{
+	public static void main(String args[])
+	{
+		int a=10,b=20;
+		int x=(a<b)?a:b; // Ternary (condition) ? true : false ;
+		System.out.println("smaller number is: "+x);
+	}
+}
+	
