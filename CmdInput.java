108aa6f05aae27cf93369ad3685e42207da0e23c
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-06-18 11:19:36 +0530

    Add files via upload

---
+++ CmdInput.java
@@ -0,0 +1,11 @@
+class CmdInput
+{
+	public static void main(String args[])
+	{
+		int a,b;
+		System.out.println("Enter two number: ");
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.println("sum of "+a+" & "+b+" is: "+(a+b));
+	}
+}
