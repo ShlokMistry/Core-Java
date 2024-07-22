0bcacc63da3d7ef56fd0398221e51dab2fb3077b
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-07-22 11:30:22 +0530

    Add files via upload

---
+++ MyArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class MyArrayList 
+{
+	public static void main(String args[]) 
+	{
+        	ArrayList<Object> A = new ArrayList<Object>();
+        	A.add(1);
+        	A.add(2);
+        	A.add("shlok");
+        	System.out.println("List is: " + A);
+    	}
+}
