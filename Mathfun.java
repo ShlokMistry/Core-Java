059d438114867f88e9a64f0eb0234fd25a6a613d
ShlokMistry <167669613+ShlokMistry@users.noreply.github.com>
2024-07-22 11:29:06 +0530

    Add files via upload

---
+++ Mathfun.java
@@ -0,0 +1,15 @@
+class Mathfun
+{
+	public static void main(String args[])
+	{
+		int a=2,b=3;
+		System.out.println(Math.pow(a,b));
+		System.out.println(Math.max(a,b));
+		System.out.println(Math.min(a,b));
+		System.out.println(Math.sqrt(a));
+	}
+}
+
+
+		
+		
